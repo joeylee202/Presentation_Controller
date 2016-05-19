36560d899a0ee56bf9d073c6dd503f58261a82fc
Joey Lee <joeylee202@gmail.com>
2016-05-19 13:45:11 -0400

    The version that could finally work

--- app/src/main/java/joeylee/com/presentationcontroller/MainActivity.java
+++ app/src/main/java/joeylee/com/presentationcontroller/MainActivity.java
@@ -1,21 +1,26 @@
 package joeylee.com.presentationcontroller;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
     Sensor mProximity;
@@ -28,65 +33,119 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     //SocketService
     SocketService ss;
+    boolean mBound = false;
 
-    public MainActivity(){
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        //Add proximity sensor
-        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        //Add accelerometer sensor
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        //initialize all the components
-        editTextAddress = (EditText) findViewById(R.id.ip_address);
-        editTextPort = (EditText) findViewById(R.id.port);
-        buttonConnect = (Button) findViewById(R.id.connectButton);
-        buttonClear = (Button) findViewById(R.id.clearButton);
-    }
+    Calendar c1r = Calendar.getInstance();
+    Calendar c1l = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        //Add proximity sensor
+        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        //Add accelerometer sensor
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
+        //initialize all the components
+        editTextAddress = (EditText) findViewById(R.id.ip_address);
+        editTextPort = (EditText) findViewById(R.id.port);
+        buttonConnect = (Button) findViewById(R.id.connectButton);
+        buttonClear = (Button) findViewById(R.id.clearButton);
 
-        buttonConnect.setOnClickListener(new View.OnClickListener() {
+
+
+        buttonConnect.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent socketIntent = new Intent(MainActivity.this, SocketService.class);
                 socketIntent.putExtra("socketAddr", editTextAddress.getText().toString());
-                socketIntent.putExtra("socketPort", editTextPort.getText().toString());
+                socketIntent.putExtra("socketPort", Integer.parseInt(editTextPort.getText().toString()));
+                MainActivity.this.startService(socketIntent);
+                MainActivity.this.bindService(socketIntent, mConnection, Context.BIND_AUTO_CREATE);
+
+             if (mBound) {
+                    Log.d("DEBUG", "Getting IP from service: " + ss.ip_address);
+                    Log.d("DEBUG", Integer.toString(ss.ip_port));
+                    ss.direction = 11;
+             }
+
+
+            }
+        });
+
+        buttonClear.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mBound){
+                    Log.d("DEBUG", "Getting IP from service: ");
+                }
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private ServiceConnection mConnection = new ServiceConnection(){
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            SocketService.LocalBinder binder = (SocketService.LocalBinder) service;
+            ss = binder.getService();
+            mBound = true;
+            Log.d("DEBUG", "service connection connected");
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d("DEBUG", "service connection disconnected");
+            ss = null;
+            mBound = false;
+        }
+    };
+
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_PROXIMITY){
+            if(event.values[0] == 0){
+                Log.d("DEBUG", "Near sensor");
+                if(mBound){
+                    ss.direction = 1;
+                }
+            }else{
+               // Log.d("DEBUG", "Far Sensor");
+            }
+        }
 
+        if(event.values[0] < -8.00){
+            Calendar c2r = Calendar.getInstance();
+            int secs1 = c1r.get(Calendar.SECOND);
+            int secs2 = c2r.get(Calendar.SECOND);
+            if(Math.abs(secs2 - secs1) > 2){
+                Log.d("DEBUG", "Right");
+                if(mBound){
+                    ss.direction = 8;
+                }
+                c1r = c2r;
+            }
+        }
+
+        if(event.values[0] > 8.00){
+            Calendar c2l = Calendar.getInstance();
+            int secs1 = c1l.get(Calendar.SECOND);
+            int secs2 = c2l.get(Calendar.SECOND);
+            if(Math.abs(secs2 - secs1) > 2){
+                Log.d("DEBUG", "Left");
+                if(mBound){
+                    ss.direction = -8;
+                }
+                c1l = c2l;
+            }
+        }
     }
 
     @Override
--- app/src/main/java/joeylee/com/presentationcontroller/SocketService.java
+++ app/src/main/java/joeylee/com/presentationcontroller/SocketService.java
@@ -4,8 +4,12 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
@@ -22,34 +26,80 @@ public class SocketService extends Service {
         }
     }
 
-    Runnable connect = new conncetSocket();
+
+    Socket clientSocket = null;
+//    DataOutputStream dos;
+    PrintWriter pw;
+    String ip_address;
+    int ip_port;
+    int direction;
+
+    Runnable connect = new connectSocket();
     Thread socketThread = new Thread(connect);
 
 
-    Socket clientSocket = null;
-    DataOutputStream dos;
-    String ip_address;
-    int ip_port;
-
-
     public void onCreate(){
-
+        Log.i("DEBUG", "Socket service created");
     }
 
     public IBinder onBind(Intent intent){
+        Log.i("DEBUG", "onBind");
         ip_address = intent.getStringExtra("socketAddr");
         ip_port = intent.getIntExtra("socketPort",0);
         return mBinder;
     }
 
     public int onStartCommand(Intent intent, int flags, int startId){
+        Log.i("DEBUG", "onStartCommand");
         ip_address = intent.getStringExtra("socketAddr");
         ip_port = intent.getIntExtra("socketPort", 0);
         socketThread.start();
+        Log.i("DEBUG", "Socket IP set to " + ip_address);
+        Log.i("DEBUG", "Socket port set to " + ip_port);
+
         return super.onStartCommand(intent, flags, startId);
     }
 
 
+class connectSocket implements Runnable{
+
+    @Override
+    public void run() {
+        try{
+            clientSocket = new Socket(ip_address, ip_port);
+            OutputStream out = clientSocket.getOutputStream();
+//            dos = new DataOutputStream(out);
+            pw = new PrintWriter(out, true);
+            Log.d("DEBUG", "sent hello");
+//            dos.writeUTF("Hello from client");
+            pw.println("hi from the client");
+
+            while(true){
+                if(direction >=8){
+                    direction = 0;
+                    Log.i("DEBUG", "change slide");
+//                    dos.writeUTF("right");
+                    pw.println("right");
+                }
+                if(direction <= -8){
+                    direction = 0;
+                    Log.i("DEBUG", "change slide");
+//                    dos.writeUTF("left");
+                    pw.println("left");
+                }
+                if(direction == 1){
+                    direction = 0;
+                    Log.i("DEBUG", "last slide");
+//                    dos.writeUTF("end");
+                    pw.println("end");
+                }
+
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
 
 
 }
